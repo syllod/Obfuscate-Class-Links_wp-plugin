9761791c8354b30c29d5e62ff05720ba1c5a923b
Sylvain L <93586077+syllod@users.noreply.github.com>
2023-05-31 15:41:24 +0300

    Create obfuscate-class-links-plugin.php
    init

---
+++ obfuscate-class-links-plugin.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Plugin Name: Obfuscate Class Links
+ * Plugin URL: https://github.com/syllod/obfuscate-class-links
+ * Description: This plugin obfuscates links within elements with the class "ob-link" using Base64 encoding.
+ * Version: 1.0.0
+ * Author: Sylvain L - Syllod
+*/
+
+function obfuscate_class_links() {
+    ?>
+    <script type="text/javascript">
+    jQuery(document).ready(function($) {
+        $('.ob-link a').each(function() {
+            var originalLink = $(this).attr('href');
+            var obfuscatedLink = 'data:text/html;base64,' + btoa(originalLink);
+            $(this).attr('href', obfuscatedLink);
+        });
+
+        $(document).on('click', '.ob-link a', function(e) {
+            e.preventDefault();
+            var obfuscatedLink = $(this).attr('href');
+            var decodedLink = obfuscatedLink.replace('data:text/html;base64,', '');
+            var originalLink = atob(decodedLink);
+            window.location.href = originalLink;
+        });
+    });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'obfuscate_class_links');
